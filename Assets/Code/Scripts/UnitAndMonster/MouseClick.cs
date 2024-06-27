5fad13a2fea0110eb757ed0022403b679537ad15
comgod <129180859+coMGod98@users.noreply.github.com>
2024-06-27 13:28:42 +0900

    UnitSelect

--- Assets/Code/Scripts/UnitAndMonster/MouseClick.cs
+++ Assets/Code/Scripts/UnitAndMonster/MouseClick.cs
@@ -10,6 +10,7 @@ public class MouseClick : MonoBehaviour
     public LayerMask unitMask;
     public LayerMask groundMask;
     public LayerMask attackMask;
+    public UnityEvent emptySelecAct;
     public UnityEvent<Unit> oneSelectAct;
     public UnityEvent<Unit> mulSelectAct;
     public UnityEvent<Vector3> moveAct;
@@ -17,24 +18,26 @@ public class MouseClick : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0)){
+
+        if (Input.GetMouseButtonDown(0)){
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, unitMask)){
-                if(hit.transform.GetComponent<Unit>() == null) return;
-                if(Input.GetKey(KeyCode.LeftControl)){
-                    mulSelectAct?.Invoke(hit.transform.GetComponent<Unit>());
-                }
-                else{
-                    oneSelectAct?.Invoke(hit.transform.GetComponent<Unit>());
-                }
+                if (hit.transform.GetComponent<Unit>() == null) return;
+                if(Input.GetKey(KeyCode.LeftControl)) mulSelectAct?.Invoke(hit.transform.GetComponent<Unit>());
+                else oneSelectAct?.Invoke(hit.transform.GetComponent<Unit>());
+            }
+            else
+            {
+                emptySelecAct?.Invoke();
             }
         }
 
-        if(Input.GetMouseButton(1)){
+        if(Input.GetMouseButtonDown(1)){
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundMask)){
                 moveAct?.Invoke(hit.point);
             }
         }
     }
+    
 }
