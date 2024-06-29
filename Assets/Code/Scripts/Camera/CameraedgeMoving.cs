a7676f2f95617bf380ca624db26efb27d8436422
tt-cookie <chkftm07@naver.com>
2024-06-29 20:17:34 +0900

    카메라
    위치이동 한계설정

--- Assets/Code/Scripts/Camera/CameraedgeMoving.cs
+++ Assets/Code/Scripts/Camera/CameraedgeMoving.cs
@@ -6,11 +6,12 @@ public class CameraedgeMoving : MonoBehaviour
 {
     public float moveSpeed = 5f; // 카메라 이동 속도
     public float boundaryThickness = 10f; // 화면 경계 두께
-
+    public Vector2 panLimit;
+    private Vector3 initialPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -59,7 +60,14 @@ public class CameraedgeMoving : MonoBehaviour
         {
             moveDirection += Vector3.forward; 
         }
-        
+
+        Vector3 newPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, initialPosition.x - panLimit.x, initialPosition.x + panLimit.x);
+        newPosition.z = Mathf.Clamp(newPosition.z, initialPosition.z - panLimit.y, initialPosition.z + panLimit.y);
+
+
+        transform.position = newPosition;
+
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
     }
 
