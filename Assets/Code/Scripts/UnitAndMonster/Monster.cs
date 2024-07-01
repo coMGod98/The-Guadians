bb4e9898e95454a4563e3735465a98a1b35d5fd2
comgod <129180859+coMGod98@users.noreply.github.com>
2024-07-01 13:40:10 +0900

    Merge in progress
    몬스터 유닛 병합중

---
+++ Assets/Code/Scripts/UnitAndMonster/Monster.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Monster : Movement
+{
+    public static List<Monster> allMonsterList = new List<Monster>();
+
+}
--- Assets/Code/Scripts/UnitAndMonster/Spawn.cs
+++ Assets/Code/Scripts/UnitAndMonster/Spawn.cs
@@ -8,6 +8,13 @@ public class Spawn : MonoBehaviour
     public Transform monsterSpawn;
     public GameObject unitPrefab;
     public GameObject monsterPrefab;
+    public Transform[] wayPoint;
+    public float spawntime = 2.0f;
+
+    void Start()
+    {
+
+    }
 
     public void SpawnUnits(){
 
@@ -15,5 +22,13 @@ public class Spawn : MonoBehaviour
         Unit unit = obj.GetComponent<Unit>();
         
         UnitController.allUnitList.Add(unit);
+    }
+
+    public void SpawnMonsters()
+    {
+        GameObject obj = Instantiate(monsterPrefab, monsterSpawn);
+        Monster monster = obj.GetComponent<Monster>();
+
+        Monster.allMonsterList.Add(monster);
     }
 }
