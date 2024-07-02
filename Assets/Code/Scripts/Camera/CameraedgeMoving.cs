5740c68462c16ddac37e3249698ab0b90a4375c3
KOSUNGHOON <dlrjsgml39@gmail.com>
2024-07-02 13:15:19 +0900

    카메라
    카메라

--- Assets/Code/Scripts/Camera/CameraedgeMoving.cs
+++ Assets/Code/Scripts/Camera/CameraedgeMoving.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraedgeMoving : MonoBehaviour
 {
     public float moveSpeed = 5f; // 카메라 이동 속도
-    public float boundaryThickness = 10f; // 화면 경계 두께
+    public float boundaryThickness= 10f; // 화면 경계 두께
     public Vector2 panLimit;
     private Vector3 initialPosition;
     // Start is called before the first frame update
@@ -20,8 +20,11 @@ public class CameraedgeMoving : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         Vector3 moveDirection = Vector3.zero;
 
-        
-  
+        Vector3 newPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, initialPosition.x - panLimit.x, initialPosition.x + panLimit.x);
+        newPosition.z = Mathf.Clamp(newPosition.z, initialPosition.z - panLimit.y, initialPosition.z + panLimit.y);
+
+
         if (mousePosition.x < boundaryThickness)
         {
             moveDirection += Vector3.left;
@@ -43,11 +46,7 @@ public class CameraedgeMoving : MonoBehaviour
             moveDirection += Vector3.forward; 
         }
 
-        Vector3 newPosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
-        newPosition.x = Mathf.Clamp(newPosition.x, initialPosition.x - panLimit.x, initialPosition.x + panLimit.x);
-        newPosition.z = Mathf.Clamp(newPosition.z, initialPosition.z - panLimit.y, initialPosition.z + panLimit.y);
-
-
+ 
         transform.position = newPosition;
 
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
