18fa8ed11a6bb88ed6de9383ebb1a97830384280
comgod <129180859+coMGod98@users.noreply.github.com>
2024-07-02 13:15:17 +0900

    Complete Merge Spawn

--- Assets/Code/Scripts/Lound.cs
+++ Assets/Code/Scripts/Lound.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class RoundTime : MonoBehaviour
+public class Lound : MonoBehaviour
 {
+    public UnityEvent startAct;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startAct?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
