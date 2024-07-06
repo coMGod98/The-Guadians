88915d6354ae4dc479afef389365803b687d4ac3
comgod <comgod98@gmail.com>
2024-07-06 17:22:37 +0900

    Add MonsterSelection

--- Assets/Code/Scripts/Monster/Monster.cs
+++ Assets/Code/Scripts/Monster/Monster.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class Monster : Movement
 {
-    public enum State{
-        Create, Normal, Battle, Death
-    }
     public static List<Monster> allMonsterList = new List<Monster>();
     Transform[] wayPoint;
     int currentWaypointIndex = 1;
 
+    public void SelectMonster(){
+        gameObject.GetComponent<Outline>().enabled = true;
+    }
+
+    public void DeselectMonster(){
+        gameObject.GetComponent<Outline>().enabled = false;
+    }
 
     public void SetWaypoint(Transform[] wayPointArray)
     {
--- Assets/Code/Scripts/Unit/Unit.cs
+++ Assets/Code/Scripts/Unit/Unit.cs
@@ -6,6 +6,7 @@ public class Unit : Movement
 {
     public static List<Unit> allUnitList = new List<Unit>();
     public GameObject unitMarker;
+    public List<float> distFromOtherUnit;
 
     public void SelectUnit(){
         unitMarker.SetActive(true);
@@ -13,7 +14,20 @@ public class Unit : Movement
     public void DeselectUnit(){
         unitMarker.SetActive(false);
     }
+    
     public void Move(Vector3 pos){
         MoveToPos(pos);
     }
+
+    private void Update() {
+        distFromOtherUnit = new List<float>();
+        foreach(Unit unit in allUnitList){
+            float dist = Vector3.Distance(unit.transform.position, transform.position);
+            distFromOtherUnit.Add(dist);
+            if(dist < 1.0f && dist > 0.1f){
+                // 유닛 간 거리가 가까우면 밀어내기
+            }
+        }
+    }
+
 }
