bb4c4c7c2bef77137e6f649949c113c1113d2cee
KOSUNGHOON <chkftm07@naver.com>
2024-07-12 12:48:10 +0900

    뽑기

---
+++ Assets/Code/Scripts/UI/SumonCircle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SumonCircle : MonoBehaviour,IPointerUpHandler,IPointerDownHandler
+{
+   public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+   public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
