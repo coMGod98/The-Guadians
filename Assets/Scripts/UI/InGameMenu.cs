25f6e0c2e4fea40c995e704b2514bd46dfc2818e
tt-cookie <chkftm07@naver.com>
2024-07-27 22:37:48 +0900

    인게임 메뉴

---
+++ Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class InGameMenu : MonoBehaviour
+{
+    public GameObject inGameMenu;
+    public GameObject optionsMenu;
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionsMenu.activeSelf)
+            {
+                optionsMenu.SetActive(false);
+            }
+            else
+            {
+                inGameMenu.SetActive(!inGameMenu.activeSelf);
+            }
+        }
+
+
+    }
+
+    public void RestartGame()
+    {
+        //재시작
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OpenOptions()
+    {
+        // 인게임 메뉴를 닫고 옵션 메뉴를 엽니다.
+        inGameMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        // 옵션 메뉴를 닫고 인게임 메뉴를 엽니다.
+        optionsMenu.SetActive(false);
+        inGameMenu.SetActive(true);
+    }
+    public void GoToMainMenu()
+    {
+        //메인메뉴
+        SceneManager.LoadScene("BeginingScene"); 
+    }
+
+    public void QuitGame()
+    {
+        //프로그램종료
+        Application.Quit();
+    }
+
+
+
+}
+
+
+
