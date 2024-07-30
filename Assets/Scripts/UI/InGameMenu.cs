fb84412f362c309e761a4edaef2ec6f9824d4ee9
KOSUNGHOON <chkftm07@naver.com>
2024-07-30 10:04:50 +0900

    인게임 메뉴

--- Assets/Scripts/UI/InGameMenu.cs
+++ Assets/Scripts/UI/InGameMenu.cs
@@ -6,41 +6,45 @@ using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
     public GameObject inGameMenu;
     public GameObject optionsMenu;
     void Update()
     {
-
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+
             if (optionsMenu.activeSelf)
             {
-                optionsMenu.SetActive(false);
-                
+                optionsMenu.SetActive(false);                
             }
             else
             {
                 inGameMenu.SetActive(!inGameMenu.activeSelf);
+                if (inGameMenu.activeSelf)
+                {
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    Time.timeScale = 1.0f;
+                }
             }
+
         }
-
-
     }
-
+    
     public void RestartGame()
     {
         //재시작
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
     public void OpenOptions()
     {
         // 인게임 메뉴를 닫고 옵션 메뉴를 엽니다.
         inGameMenu.SetActive(false);
         optionsMenu.SetActive(true);
     }
-
     public void CloseOptions()
     {
         // 옵션 메뉴를 닫고 인게임 메뉴를 엽니다.
@@ -52,7 +56,6 @@ public class InGameMenu : MonoBehaviour
         //메인메뉴
         SceneManager.LoadScene("BeginingScene"); 
     }
-
     public void QuitGame()
     {
         //프로그램종료
