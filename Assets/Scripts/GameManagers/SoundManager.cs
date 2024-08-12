0fd95214195ab85823bbaedd625537d70c78973f
coMGod98 <129180859+coMGod98@users.noreply.github.com>
2024-08-12 12:21:56 +0900

    Sound 완료
    AOE만 하면됨

--- Assets/Scripts/GameManagers/SoundManager.cs
+++ Assets/Scripts/GameManagers/SoundManager.cs
@@ -17,4 +17,25 @@ public class SoundManager : MonoBehaviour
         AudioSource[] temp = GetComponentsInChildren<AudioSource>();
         goldAudio = temp[1];
     }
+
+
+    public void MASTERSetLevel(float sliderval)
+    {
+        audioMixer.SetFloat("MASTER", Mathf.Log10(sliderval) * 20);
+    }
+
+    public void BGMSetLevel(float sliderval)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(sliderval) * 20);
+    }
+
+    public void SYSTEMSetLevel(float sliderval)
+    {
+        audioMixer.SetFloat("SYSTEM", Mathf.Log10(sliderval) * 20);
+    }
+
+    public void SFXSetLevel(float sliderval)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(sliderval) * 20);
+    }
 }
